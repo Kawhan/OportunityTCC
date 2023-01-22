07c97ba111c82c7ccaab0a90c96117e8bef49f05
Kawhan <kawhanlaurindo@outlook.com>
2023-01-21 21:18:24 -0300

    commit_21/01/2023_complet

--- projectDJANGO/opportunity/opportunity/urls.py
+++ projectDJANGO/opportunity/opportunity/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework import routers
 from apps.project.views import AlunosViewSet, InscricaoViewSet, ProfessorViewSet, vagasEmpregoViewSet, areaInteresseViewSet, professorVagasCadastradasViewSet, professorAreaInteresseViewSet, ListaIncricoesAlunoViewSet, ListaInscricoesVagaViewSet
 
@@ -9,13 +12,19 @@ router.register('alunos', AlunosViewSet, basename="Alunos")
 router.register('inscricoes', InscricaoViewSet, basename="Inscricoes")
 router.register('professor', ProfessorViewSet, basename="Professores")
 router.register('vagas', vagasEmpregoViewSet, basename="Vagas")
-router.register('areaInteresse', areaInteresseViewSet, basename="AreaInteresse")
+router.register('areaInteresse', areaInteresseViewSet,
+                basename="AreaInteresse")
 
 urlpatterns = [
-     path('admin/', admin.site.urls),
-     path('', include(router.urls)),
-     path('professor/<int:pk>/cadastros/', professorVagasCadastradasViewSet.as_view()),
-     path('professor/<int:pk>/interesses/', professorAreaInteresseViewSet.as_view()),
-     path('aluno/<int:pk>/inscricoes/', ListaIncricoesAlunoViewSet.as_view()),
-     path('vaga/<int:pk>/inscricoes/', ListaInscricoesVagaViewSet.as_view())
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('professor/<int:pk>/cadastros/',
+         professorVagasCadastradasViewSet.as_view()),
+    path('professor/<int:pk>/interesses/',
+         professorAreaInteresseViewSet.as_view()),
+    path('aluno/<int:pk>/inscricoes/', ListaIncricoesAlunoViewSet.as_view()),
+    path('vaga/<int:pk>/inscricoes/', ListaInscricoesVagaViewSet.as_view())
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
