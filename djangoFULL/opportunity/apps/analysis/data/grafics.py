03c97ee88caedee54899476fe0e6aa6d0769d40c
Kawhan <kawhanlaurindo@outlook.com>
2023-03-17 09:25:38 -0300

    Refactor code grafics and add new statistic from students
    Add new class Start
    Add all subscribe in extension project
    Add all subscribe in intern jobs
    Add new grafics for new statistic
    
    Refactor grafics code
    
    Signed-off-by: Kawhan <kawhanlaurindo@outlook.com>

---
+++ djangoFULL/opportunity/apps/analysis/data/grafics.py
@@ -0,0 +1,122 @@
+from project.models import vagasEmprego
+
+
+class Start:
+    def define_data_info(data: dict):
+        data['alunos_SI_pesquisa'] = 0
+        data['alunos_LCC_pesquisa'] = 0
+
+        data['inscricoes_SI_pesquisa'] = 0
+        data['inscricoes_LCC_pesquisa'] = 0
+
+        data['alunos_SI_extensao'] = 0
+        data['alunos_LCC_extensao'] = 0
+
+        data['inscricoes_SI_extensao'] = 0
+        data['inscricoes_LCC_extensao'] = 0
+
+        data['alunos_SI_estagio'] = 0
+        data['alunos_LCC_estagio'] = 0
+
+        data['inscricoes_SI_estagio'] = 0
+        data['inscricoes_LCC_estagio'] = 0
+
+        return data
+
+    def define_info_count_from_jobs(data: dict):
+        data['PP'] = vagasEmprego.objects.filter(tipo_vaga='PP').count()
+        data['PE'] = vagasEmprego.objects.filter(tipo_vaga='PE').count()
+        data['ES'] = vagasEmprego.objects.filter(tipo_vaga='ES').count()
+
+        return data
+
+    def query_set_info_from_jobs(query_set_data: dict):
+        query_set_data['queryset_pesquisa'] = vagasEmprego.objects.filter(
+            tipo_vaga='PP')
+        query_set_data['queryset_extensao'] = vagasEmprego.objects.filter(
+            tipo_vaga='PE')
+        query_set_data['queryset_estagio'] = vagasEmprego.objects.filter(
+            tipo_vaga='ES')
+
+        return query_set_data
+
+    def cal_info_students_in_extension_project(query_set_data: dict, alunos_extensao: list, data: dict):
+        for extensao in query_set_data['queryset_extensao']:
+            for aluno in extensao.aluno.values().distinct():
+                if aluno['curso'] == 'SI' and aluno not in alunos_extensao:
+                    alunos_extensao.append(aluno)
+                    data['alunos_SI_extensao'] += 1
+                elif aluno['curso'] == 'LCC' and aluno not in alunos_extensao:
+                    alunos_extensao.append(aluno)
+                    data['alunos_LCC_extensao'] += 1
+
+        return {
+            'alunos_extensao': alunos_extensao,
+            'data': data
+        }
+
+    def all_subscribe_in_extesion_project(query_set_data: dict, alunos_extensao: list, data: dict):
+        for extensao in query_set_data['queryset_extensao']:
+            for aluno in extensao.aluno.values().distinct():
+                if aluno['curso'] == 'SI':
+                    data['inscricoes_SI_extensao'] += 1
+                elif aluno['curso'] == 'LCC':
+                    data['inscricoes_LCC_extensao'] += 1
+
+        return {
+            'data': data
+        }
+
+    def cal_info_stundents_in_research_project(query_set_data: dict, alunos_pesquisa: list, data: dict):
+        for pesquisa in query_set_data['queryset_pesquisa']:
+            for aluno in pesquisa.aluno.values().distinct():
+                if aluno['curso'] == 'SI' and aluno not in alunos_pesquisa:
+                    alunos_pesquisa.append(aluno)
+                    data['alunos_SI_pesquisa'] += 1
+                if aluno['curso'] == 'LCC' and aluno not in alunos_pesquisa:
+                    alunos_pesquisa.append(aluno)
+                    data['alunos_LCC_pesquisa'] += 1
+
+        return {
+            'alunos_pesquisa': alunos_pesquisa,
+            'data': data
+        }
+
+    def all_subscribe_in_research_project(query_set_data: dict, alunos_pesquisa: list, data: dict):
+        for pesquisa in query_set_data['queryset_pesquisa']:
+            for aluno in pesquisa.aluno.values().distinct():
+                if aluno['curso'] == 'SI':
+                    data['inscricoes_SI_pesquisa'] += 1
+                if aluno['curso'] == 'LCC':
+                    data['inscricoes_LCC_pesquisa'] += 1
+
+        return {
+            'data': data
+        }
+
+    def cal_info_students_in_intern(query_set_data: dict, alunos_estagio: list, data: dict):
+        for estagio in query_set_data['queryset_estagio']:
+            for aluno in estagio.aluno.values().distinct():
+                if aluno['curso'] == 'SI' and aluno not in alunos_estagio:
+                    alunos_estagio.append(aluno)
+                    data['alunos_SI_estagio'] += 1
+                elif aluno['curso'] == 'LCC' and aluno not in alunos_estagio:
+                    alunos_estagio.append(aluno)
+                    data['alunos_LCC_estagio'] += 1
+
+        return {
+            'alunos_estagio': alunos_estagio,
+            'data': data
+        }
+
+    def all_subscribe_in_intern(query_set_data: dict, alunos_estagio: list, data: dict):
+        for estagio in query_set_data['queryset_estagio']:
+            for aluno in estagio.aluno.values().distinct():
+                if aluno['curso'] == 'SI':
+                    data['inscricoes_SI_estagio'] += 1
+                if aluno['curso'] == 'LCC':
+                    data['inscricoes_LCC_estagio'] += 1
+
+        return {
+            'data': data
+        }
--- djangoFULL/opportunity/apps/analysis/views/views.py
+++ djangoFULL/opportunity/apps/analysis/views/views.py
@@ -1,4 +1,5 @@
 from accounts.models import UserProfile
+from analysis.data.grafics import Start
 from django.shortcuts import render
 from project.models import vagasEmprego
 
@@ -12,65 +13,36 @@ def analises(request):
 
     labels = []
     data = {}
-    data['alunos_SI_pesquisa'] = 0
-    data['alunos_LCC_pesquisa'] = 0
+    data = Start.define_data_info(data)
+    data = Start.define_info_count_from_jobs(data)
 
-    data['inscricoes_SI_pesquisa'] = 0
-    data['inscricoes_LCC_pesquisa'] = 0
+    query_set_data = {}
 
-    data['alunos_SI_extensao'] = 0
-    data['alunos_LCC_extensao'] = 0
-
-    data['alunos_SI_estagio'] = 0
-    data['alunos_LCC_estagio'] = 0
-
-    queryset = vagasEmprego.objects.all()
-    data['PP'] = vagasEmprego.objects.filter(tipo_vaga='PP').count()
-    data['PE'] = vagasEmprego.objects.filter(tipo_vaga='PE').count()
-    data['ES'] = vagasEmprego.objects.filter(tipo_vaga='ES').count()
-
-    queryset_pesquisa = vagasEmprego.objects.filter(tipo_vaga='PP')
-
-    queryset_extensao = vagasEmprego.objects.filter(tipo_vaga='PE')
-
-    queryset_estagio = vagasEmprego.objects.filter(tipo_vaga='ES')
+    query_set_data = Start.query_set_info_from_jobs(query_set_data)
 
     # Quantidade alunos inscritos em pesquisas
-    for pesquisa in queryset_pesquisa:
-        for aluno in pesquisa.aluno.values().distinct():
-            if aluno['curso'] == 'SI' and aluno not in alunos_pesquisa:
-                alunos_pesquisa.append(aluno)
-                data['alunos_SI_pesquisa'] += 1
-            if aluno['curso'] == 'LCC' and aluno not in alunos_pesquisa:
-                alunos_pesquisa.append(aluno)
-                data['alunos_LCC_pesquisa'] += 1
-            if aluno['curso'] == 'SI':
-                data['inscricoes_SI_pesquisa'] += 1
-            if aluno['curso'] == 'LCC':
-                data['inscricoes_LCC_pesquisa'] += 1
+    alunos_pesquisa = Start.cal_info_stundents_in_research_project(
+        query_set_data, alunos_pesquisa, data)['alunos_pesquisa']
+    data = Start.cal_info_stundents_in_research_project(
+        query_set_data, alunos_pesquisa, data)['data']
+    data = Start.all_subscribe_in_research_project(
+        query_set_data, alunos_pesquisa, data)['data']
 
     # Quntidade alunos inscritos em extensão
-    for extensao in queryset_extensao:
-        for aluno in extensao.aluno.values().distinct():
-            if aluno['curso'] == 'SI' and aluno not in alunos_extensao:
-                alunos_extensao.append(aluno)
-                data['alunos_SI_extensao'] += 1
-            elif aluno['curso'] == 'LCC' and aluno not in alunos_extensao:
-                alunos_extensao.append(aluno)
-                data['alunos_LCC_extensao'] += 1
+    alunos_extensao = Start.cal_info_students_in_extension_project(
+        query_set_data, alunos_extensao, data)['alunos_extensao']
+    data = Start.cal_info_students_in_extension_project(
+        query_set_data, alunos_extensao, data)['data']
+    data = Start.all_subscribe_in_extesion_project(
+        query_set_data, alunos_extensao, data)['data']
 
     #  Quantidade alunos em estágio
-    for estagio in queryset_estagio:
-        for aluno in estagio.aluno.values().distinct():
-            if aluno['curso'] == 'SI' and aluno not in alunos_estagio:
-                alunos_estagio.append(aluno)
-                data['alunos_SI_estagio'] += 1
-            elif aluno['curso'] == 'LCC' and aluno not in alunos_estagio:
-                alunos_estagio.append(aluno)
-                data['alunos_LCC_estagio'] += 1
-
-        # data['alunos_SI_pesquisa'] = pesquisa.aluno.filter()
-        # data['alunos_LCC_pesquisa'] = pesquisa.aluno.filter()
+    alunos_estagio = Start.cal_info_students_in_intern(
+        query_set_data, alunos_estagio, data)['alunos_estagio']
+    data = Start.cal_info_students_in_intern(
+        query_set_data, alunos_estagio, data)['data']
+    data = Start.all_subscribe_in_intern(
+        query_set_data, alunos_estagio, data)['data']
 
     # Quantidade inscritos em pesquisa
 
